f4e52d788f6b0283ce85209b1b3c8afce5465785
Hui <jeffrey.x.hui@gmail.com>
2020-01-29 20:15:03 -0800

    token authorization working as intended. That's MVP!

--- middleware/restricted-middleware.js
+++ middleware/restricted-middleware.js
@@ -1,5 +1,21 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 function restrict(req, res, next) {
-  next();
+  if(req.url.includes('restricted')) {
+    const authorization = req.headers.authorization;
+    const token = jwt.verify(authorization, process.env.JWT_SECRET);
+    if(token) {
+      req.token = token;
+      next();
+    } else {
+      res.status(401).json({ message: `status 401: token authorization failed` })
+    }
+  } else {
+    next();
+  }
+
+
 }
 
 module.exports = restrict;
